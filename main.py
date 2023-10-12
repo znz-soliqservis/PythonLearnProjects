fa3e838b93abd6bbefd7b51c57ab96213038c86a
nzulkaynarov <25324275-nzulkaynarov@users.noreply.replit.com>
2023-10-12 19:32:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print(type(2/1))
+print(type(2/5))
+print(type(2/-1))
+print(type(2/2))
+print(type(2/-2))
+print(type(2/3))
+print(type(2/-3))
+print(type(2/4))
+print(type(2/-4))
+print(type(2/5))
+print(type(2/-5))
+print(type(2/6))
+
