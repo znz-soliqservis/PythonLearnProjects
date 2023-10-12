f897fc5bedd0c6ef1080591aeb0d9f47613010d2
nzulkaynarov <87830164+nzulkaynarov@users.noreply.github.com>
2023-10-12 21:07:39 +0000

    Надо разобраться
    в файле hello

--- Udemy/hello.py
+++ Udemy/hello.py
@@ -1,6 +1,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    
+  print('Hi, ' {name}) 
+  
+# Press Ctrl+F8 to toggle the breakpoint.
+name = input("pidr")
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm pidr')
