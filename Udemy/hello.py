9c5cdc8de3341aa96e0d9dffc904904c5d62a348
Soliq Servis <devsoliqservice@gmail.com>
2023-11-23 11:48:37 +0500

    скобки

--- Udemy/hello.py
+++ Udemy/hello.py
@@ -1,7 +1,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     
-  print ( 'Hi, ' {name}) 
+  print('Hi, ' {name}) 
   
 # Press Ctrl+F8 to toggle the breakpoint.
 name = input("hello")
