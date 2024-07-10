f69566e7c074283cac4b2b938c31c33c44ae3529
Nurbek Soliqservis <n.zulkaynarov@soliqservis.uz>
2024-07-10 15:56:28 +0500

    го

--- Udemy/hello.py
+++ Udemy/hello.py
@@ -1,12 +1,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    
-  print('Hi, ' {name}) 
-  
+    print(f'Hi, {name}')  # Исправлено форматирование строки
+
 # Press Ctrl+F8 to toggle the breakpoint.
-name = input("hello")
+name = input("hello: ")  # Добавлено двоеточие для ясности приглашения
 if __name__ == '__main__':
     print_hi('PyCharm norm')
-		print_hi(name)
-
-#проверка кода
+    print_hi(name)  # Исправлены отступы
